229fa7b286b13dc8524fd56726624ba2b1a85ebf
ThanhDat22 <107126168+ThanhDat22@users.noreply.github.com>
2022-09-29 11:40:02 -0500

    Update Polynomial.h

--- Polynomial.h
+++ Polynomial.h
@@ -1,1 +1,30 @@
-#pragma once
+#ifndef POLYNOMIAL_H
+#define POLYNOMIAL_H
+
+#include<iostream>
+
+
+/*A Polynomial which is a doubly-linked list of Terms.*/
+class Term {
+public:
+	// Data fields
+	int coefficient, exponent; // Stores coefficient and exponent of Polynomial
+	Term* next; // A pointer to the next term in the Polynomial
+	Term* prev; // A pointer to the previous term in the Polynomial
+
+	// Constructor
+	Term(const int&, const int&);
+};
+//Implementation of class {Term}
+
+// Default constructor
+Term::Term(const int& coefficient, const int& exponent): coefficient(coefficient), exponent(exponent), next(NULL), prev(NULL) {}
+
+class Polynomial {
+public:
+private:
+	/*A Term of Polynomial*/
+	struct Term;
+
+};
+#endif
