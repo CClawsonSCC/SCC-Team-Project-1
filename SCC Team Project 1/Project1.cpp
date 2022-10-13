8626607e756c3e3955edfc9a4ff23c43ba49cbc2
ZackLynx <71418708+CClawsonSCC@users.noreply.github.com>
2022-10-13 17:28:00 -0500

    Initial upload of UI.

---
+++ SCC Team Project 1/Project1.cpp
@@ -0,0 +1,23 @@
+// Project1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+string polynomial1;
+string polynomial2;
+
+
+	cout <<" please enter the first polynomial : " << endl;
+	cin >> polynomial1;
+    cout << "please enter the second polynomial : " << endl;
+	cin >> polynomial2;
+
+		return 0;
+
+}
+
+
