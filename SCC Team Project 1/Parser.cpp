30cef067c23bd1aefa0c0cac3eddf64386048276
ZackLynx <71418708+CClawsonSCC@users.noreply.github.com>
2022-10-22 20:03:13 -0500

    Added code comments to Parser class, removed redundant lines of code.

--- SCC Team Project 1/Parser.cpp
+++ SCC Team Project 1/Parser.cpp
@@ -1,5 +1,4 @@
 #include "Parser.h"
-#include "Windows.h"
 
 using std::exception;
 using std::to_string;
@@ -11,7 +10,7 @@ using std::to_string;
 */
 void Parser::assign_first(const string in_string)
 {
-	read_input(in_string, poly1); // this will assign terms to the chosen list.
+	read_input(in_string, poly1);
 }
 
 /**
@@ -30,11 +29,11 @@ void Parser::assign_second(const string in_string)
 */
 void Parser::calculate_result()
 {
-	results = poly1;
-	while (!poly2.empty())
+	results = poly1; // bring over the first polynomial.
+	while (!poly2.empty()) // while the second polynomial has elements.
 	{
-		insert(poly2.front(), results);
-		poly2.pop_front();
+		insert(poly2.front(), results); // use insert() to sum the terms.
+		poly2.pop_front(); // remove the now added term from the second polynomial.
 	}
 }
 
@@ -44,35 +43,33 @@ void Parser::calculate_result()
 */
 string Parser::print()
 {
-	
-	if (results.empty()) // Polynomial is empty.
-	{
-		return "Empty Polynomial.";
-	}
-	//return to_string(poly1.size());
+	// Polynomial is empty.
+	if (results.empty()) { return "Empty Polynomial."; }
 
 	// polynomial is NOT empty.
-	
 	string result_str = "";
+
+	// Using iterators, go through each term and concatenate the values to a string. 
 	for (list<Term>::iterator it = results.begin(); it != results.end(); it++)
 	{
-		if ((it != results.begin()) && (it->get_coefficient() > 0))
-		{
-			result_str.append("+");
-		}
+		if (it->get_coefficient() == 0)	{ continue; } // If a result term solved to 0, ignore it.
 
+		// It's not the first term and the coeficient is positive.
+		if ((it != results.begin()) && (it->get_coefficient() > 0))	{ result_str.append("+"); }
+
+		// The coefficient is neither 1 or -1, or the exponent is 0.
 		if (((it->get_coefficient() != 1) && (it->get_coefficient() != -1)) || (it->get_exponent() == 0))
 		{
 			result_str.append(to_string(it->get_coefficient()));
 		}
 
-		if (it->get_coefficient() == -1)
-		{
-			result_str.append("-");
-		}
+		// Coeficient is -1.
+		if (it->get_coefficient() == -1) { result_str.append("-"); }
 
+		// The exponent isn't 0.
 		if (it->get_exponent() != 0)
 		{
+			// Output the variable. If the exponent is not 1, output the exponent.
 			result_str.append("x" + (it->get_exponent() != 1 ? ("^" + to_string(it->get_exponent())) : ""));
 		}
 	}
@@ -83,79 +80,74 @@ string Parser::print()
 // Private Functions
 
 /**
- * @brief Parses the user inputed polynomial into a useable 
+ * @brief Parses the user inputed polynomial into a useable list of terms.
  * @param in_string - User input string.
  * @param polynomial - the list to record the polynomial to.
 */
 void Parser::read_input(const string in_string, list<Term>& polynomial)
 {
-	//Todo Implement powers of 1 support.
-	int coeficient = 0;
+	// Function data fields.
+	int coefficient = 0;
 	int exponent = 0;
 	bool is_negative = false;
 	bool is_exponent = false;
 
-	polynomial.clear();
+	polynomial.clear(); // Clear the list of any left over terms.
 
+	// Iterate through the string
 	for (size_t i = 0; i < in_string.size(); i++)
-	{		
-		char element = in_string.at(i);
+	{
+		char element = in_string.at(i); // Get the character at the current index.
 
-		if (element == '-')	{ is_negative = true; }
-		else if (element == '+') { is_negative = false; }
-		else if (element == '^') {
+		if (element == '-')	{ is_negative = true; }  // The next value is negative.
+		else if (element == '+') { continue; } // It is asumed the next value is positive.
+		else if (element == '^') { // The next value is an exponent.
 			is_exponent = true;
-			is_negative = false;
 		}
-		else if (element == 'x') 
+		else if (element == 'x') // Character is a variable.
 		{
 			//if no coeficient given
 			if ((i == 0) || ((in_string.at(i-1) == '+') || (in_string.at(i - 1) == '-')))
 			{
-				coeficient = 1 * (is_negative ? -1 : 1);
+				coefficient = 1 * (is_negative ? -1 : 1);
 			}
 
 			//if no exponent given
 			if ((i == in_string.size() - 1) || (in_string.at(i + 1) != '^'))
 			{
-				insert(Term(coeficient, 1), polynomial);
+				insert(Term(coefficient, 1), polynomial);
 			}
 		}
 		else if ((element >= '0') && (element <= '9')) // Character is a numeric value
 		{
-			int val = in_string.at(i) - 48;
+			int val = in_string.at(i) - 48; // Subtract the character by 0's ascii value of 48.
+
+			// Iterate through and get the entire value.
 			while ((i != in_string.size() - 1) && ((in_string.at(i + 1) >= '0') && (in_string.at(i + 1) <= '9')))
 			{
 				i++;
 				val = (val * 10) + (in_string.at(i) - 48);
 			}
 			
-			val *= is_negative ? -1 : 1;
+			val *= is_negative ? -1 : 1; // If the value entered was negative, make it negative.
 			
+			// assign the value to either the coefficient or the exponent.
 			if (is_exponent) { exponent = val; }
-			else { coeficient = val; }
+			else { coefficient = val; }
 
-			if (exponent != 0) 
-			{ 
-				//insert(Term(coeficient, exponent), polynomial);
-				insert(Term(coeficient, exponent), polynomial);
-			}
-			
-			// 1x^3+1x+5
-			else if ((i <= in_string.size() - 3) && (element == 'x'))
-			{
-				exponent = 1;
-			}
+			// If the exponent is not 0, add it to the list.
+			if (exponent != 0) { insert(Term(coefficient, exponent), polynomial); }
 
 			// if we are at the end of the string or the next character is + or -...
 			else if ((i == in_string.size() - 1) ||
 				((in_string.at(i + 1) == '+') || (in_string.at(i + 1) == '-')))
 			{    
-				// term is a whole number or last number
-				//insert(Term(coeficient, 0), polynomial);
-				insert(Term(coeficient, 0), polynomial);
+				// term is a whole number or last number, add it to the list.
+				insert(Term(coefficient, 0), polynomial);
 			}
 
+			// Reset values for the next loop
+			coefficient = 0;
 			exponent = 0;
 			is_exponent = false;
 			is_negative = false;
@@ -167,49 +159,55 @@ void Parser::read_input(const string in_string, list<Term>& polynomial)
  * @brief Iterate through the list and inserts the term into its ordered place.
  * @param term - the newly created term.
  * @param polynomial - linked list of polynomials.
- * @todo: create and test this function.
 */
 void Parser::insert(Term term, list<Term>& polynomial)
 {
-	// terms with a coeficient of 0 always evaluate to 0, so don't add anything!
+	// Terms with a coeficient of 0 always evaluate to 0, so don't add anything!
 	if (term.get_coefficient() == 0) { return; }
 
-	// otherwise, add the term.
+	// Otherwise, add the term.
 	if (polynomial.empty())	{ polynomial.push_back(term); }
 
+	// If there is only one term in the list...
 	else if (polynomial.size() == 1)
 	{
-		// they share the same exponent.
+		// and they share the same exponent, add the terms.
 		if (polynomial.front().get_exponent() == term.get_exponent())
 		{
 			polynomial.front() = term + polynomial.front();
 		}
 
-		else if (polynomial.front().get_exponent() < term.get_exponent()) // the new term has larger exponent...
+		// and the new term has larger exponent, push it to the front.
+		else if (polynomial.front().get_exponent() < term.get_exponent())
 		{
 			polynomial.push_front(term);
 		}
-		else // the new term has a smaller exponent...
+		else // and the new term has a smaller exponent, push it to the back.
 		{
 			polynomial.push_back(term);
 		}
 	}
-	else
+	else // There's more than one term in the list. 
 	{
+		// Go through the list to find it's proper spot.
 		for (list<Term>::iterator it = polynomial.begin(); it != polynomial.end(); it++)
 		{
-			if (it->get_exponent() == term.get_exponent())
+			// A like term has been found. Add them.
+			if (it->get_exponent() == term.get_exponent()) 
 			{
 				*it = term + *it;
 				return;
 			}
-			else if (it->get_exponent() < term.get_exponent())
+
+			// the term is higher than the one at the iterator.
+			else if (it->get_exponent() < term.get_exponent()) 
 			{
 				polynomial.insert(it, term);
 				return;
 			}
 		}
 
+		// If the term is the lowest, push it to the back.
 		if (polynomial.back().get_exponent() > term.get_exponent())
 		{
 			polynomial.push_back(term);
--- SCC Team Project 1/Parser.h
+++ SCC Team Project 1/Parser.h
@@ -1,4 +1,5 @@
-#pragma once
+#ifndef PARSER_H
+#define PARSER_H
 
 #include "Term.h"
 
@@ -8,21 +9,31 @@
 using std::list;
 using std::string;
 
+/**
+ * @brief Parser class that holds three ordered linked lists of terms.
+ * This class is to be used by the main function.
+ *
+ * @author Connor Clawson
+*/
 class Parser
 {
 public:
+	// Member functions.
 	void assign_first(const string);
 	void assign_second(const string);
 	void calculate_result();
 	string print();
 
 private:
-	//replace <int> with <Term> soon.
+	// Data fields.
 	list<Term> poly1;
 	list<Term> poly2;
 	list<Term> results;
 	list<int> something;
-	
+
+	// Member functions.
 	void read_input(const string, list<Term>&);
 	void insert(Term, list<Term>&);
 };
+
+#endif // !PARSER_H
